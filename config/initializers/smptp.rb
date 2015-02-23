f2ba012f3541217f5f21f350aea07747305e8218
gokul <gokulraja27@gmail.com>
2015-02-23 11:38:48 -0500

    change in smtp

--- config/initializers/smptp.rb
+++ config/initializers/smptp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "cab",
-  :user_name            => "",
-  :password             => "",
+  :user_name            => "mothitravelscab@gmail.com",
+  :password             => "mothitravels",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
