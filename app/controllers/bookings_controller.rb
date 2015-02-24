ff03ba411da47695908d801013071bbf1b13e2f0
Mothirajha <rajha12@gmail.com>
2015-02-25 00:07:50 +0530

    [Authorization] roles and permissions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+  load_and_authorize_resource
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
 
   # GET /bookings
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  has_and_belongs_to_many :roles
+
+  before_save :set_role
+
+  def set_role
+    if self.role_ids.empty?
+      self.role_ids = Role.find_or_create_by(name: "user").id
+    end
+  end
+
+  def admin
+    if self.roles.map(&:name).include?("admin")
+      return true
+    end
+  end
+
 end
